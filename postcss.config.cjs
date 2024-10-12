84dc9406255d5c61540056fa1343f1f5d2321255
Srihari R <120409108+srihari-976@users.noreply.github.com>
2024-10-12 09:42:09 +0530

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,24 @@
+const path = require('path');
+
 module.exports = {
-    plugins: {
-        autoprefixer: {},
-    }
-}
+  // other settings...
+  entry: './src/index.js', // your entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // output directory
+    filename: 'bundle.js', // output bundle name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+        exclude: /node_modules\/@react-three\/drei\/node_modules\/@mediapipe/,
+      },
+      // other rules...
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.mjs'], // extensions webpack will resolve
+  },
+};
